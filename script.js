de187ff50a6271f0c521d7364343da60ad3eaa33
oscar-wolfbrandt <oscar.schmererwolfbrandt@elev.ga.ntig.se>
2024-05-24 14:15:09 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function hamburgerMenu() {
+    const primaryNavigation = document.getElementById("primary-navigation");
+    const btnHamburger = document.getElementById("hamburger-button");
+
+    btnHamburger.addEventListener("click", () => {
+        primaryNavigation.classList.toggle("open");
+        btnHamburger.setAttribute("aria-expanded", btnHamburger.getAttribute("aria-expanded") === "false" ? "true" : "false");
+    })
+}
+
+hamburgerMenu();
